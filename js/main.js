b8be53d3ede556133c83b8cd84a7adf879fdbdfc
kevin.torrington@gmail.com <kevin@kevins-Mac-mini.local>
2019-12-24 23:52:49 +0000

    Changed all pages.

--- js/main.js
+++ js/main.js
@@ -326,3 +326,20 @@
         initDropdown();
    
 })( jQuery );
+
+// Back to top
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
